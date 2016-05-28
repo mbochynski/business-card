d5c2d717baeccd01eeeb23d0d7195bfcfe169aef
mbochynski <mbochynski@MacBook-Pro-mbochynski.local>
2016-05-28 13:09:04 +0200

    Fix Navbar behaviour with height 100%

--- grav/user/themes/antimatter/js/antimatter.js
+++ grav/user/themes/antimatter/js/antimatter.js
@@ -2,18 +2,24 @@ var isTouch = window.DocumentTouch && document instanceof DocumentTouch;
 
 function scrollHeader() {
     // Has scrolled class on header
+    console.log('scrollHeader triggered');
     var zvalue = $(document).scrollTop();
-    if ( zvalue > 75 )
-        $("#header").addClass("scrolled");
-    else
-        $("#header").removeClass("scrolled");
+    if ( zvalue > 75 ) {
+      $("#header").addClass("scrolled");
+      console.log('scrolled', zvalue);
+    } else {
+      $("#header").removeClass("scrolled");
+      console.log('remove scrolled', zvalue);
+    }
 }
 
 jQuery(document).ready(function($){
 
     // ON SCROLL EVENTS
     if (!isTouch){
+      console.log('register scroll listeter');
         $(document).scroll(function() {
+          console.log('registed scroll listener triggered');
             scrollHeader();
         });
     };
@@ -25,13 +31,6 @@ jQuery(document).ready(function($){
         }
     });
 
-    //Smooth scroll to top
-    $('#toTop').click(function(){
-        $("html, body").animate({ scrollTop: 0 }, 500);
-        return false;
-    });
     // Responsive Menu
 
 });
-
-
